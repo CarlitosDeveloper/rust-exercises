19166b291ca9931aba521f46a989add973596796
CarlitosDeveloper <LearnWithCarlitos@gmail.com>
2025-04-01 20:11:23 -0500

    hello world 2

--- src/bin/1-Hello_World.rs
+++ src/bin/1-Hello_World.rs
@@ -2,7 +2,22 @@
 // 📌 Yo no soy el autor del enunciado, solo de la solución implementada.
 // 🎓 Propósito: aprendizaje personal del lenguaje Rust.
 
+// https://exercism.org/tracks/rust/exercises/hello-world
+
+// Versión Plataforma --> El código está comentado, solo descoméntalo y úsalo en la plataforma
+
 // &'static is a "lifetime specifier", something you'll learn more about later
-pub fn hello() -> &'static str {
-    "Hello, World!"
+
+// pub fn hello() -> &'static str {
+//     "Hello, World!"
+// }
+
+// Version Carlitos -->
+
+fn hello() {
+    println!("Hello, World!")
+}
+
+fn main() {
+    hello();
 }
